0b19eb0c4fbf1f0d0bca28fc4c5f43ea43646dd4
Patrick Gidich <patrick@simnova.com>
2021-09-02 13:50:00 -0400

    tweak

--- data-access/jest.config.js
+++ data-access/jest.config.js
@@ -1,10 +1,9 @@
-// jest.config.js
-
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    roots: ["<rootDir>/."],
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/**/*.test.ts'],
+  roots: ["<rootDir>/."],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.test.ts'],
     coverageReporters: ['json', 'lcov'],
     coverageDirectory: 'coverage',
     collectCoverageFrom: [
@@ -12,11 +11,11 @@ module.exports = {
         "!**/node_modules/**",
         "!**/dist/**"
       ]
-}
+};
 
 process.env = Object.assign(process.env, {    
-    APPINSIGHTS_INSTRUMENTATIONKEY: "95c3d0e3-d463-4a8d-a496-f22e9806bb8b",
-    WEBSITE_HOSTNAME: "localhost",
-    BASIC_AUTH_USERNAME: "testUser",
-    BASIC_AUTH_PASSWORD: "testPassword",
+  APPINSIGHTS_INSTRUMENTATIONKEY: "95c3d0e3-d463-4a8d-a496-f22e9806bb8b",
+  WEBSITE_HOSTNAME: "localhost",
+  BASIC_AUTH_USERNAME: "testUser",
+  BASIC_AUTH_PASSWORD: "testPassword",
 })
