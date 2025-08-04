eb23b0e772cded455f54ffe62974c125fae8185b
Jason Morais <jasonmorais02@gmail.com>
2025-08-04 11:53:29 -0400

    remove untracked folder

--- docusaurus/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Volumes/files/src/sharethrift/docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/Volumes/files/src/sharethrift/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Volumes/files/src/sharethrift/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Volumes/files/src/sharethrift/docusaurus/src/css/custom.css"),
-];
