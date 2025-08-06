1848aa0b07eb7296eb30ac69307d628bb16055e7
Jason Morais <jasonmorais02@gmail.com>
2025-08-06 13:48:35 -0400

    fixed router wrapper and brought over apollo client

--- packages/ui-sharethrift/src/components/layouts/home/index.tsx
+++ packages/ui-sharethrift/src/components/layouts/home/index.tsx
@@ -2,20 +2,23 @@
 import { Route, Routes } from "react-router-dom";
 import AccountRoutes from "./account";
 import AdminDashboardRoutes from "./admin-dashboard";
-import MessagesRoutes from "./messages/Index";
-import MyListingsRoutes from "./my-listings/Index";
 import MyReservationsRoutes from "./my-reservations/Index";
 import Listings from "./components/Listings";
+import HomeTabsLayout from "./section-layout";
+import MyListingsRoutes from "./my-listings/Index";
+import MessagesRoutes from "./messages/Index";
 
 export default function HomeRoutes() {
   return (
     <Routes>
-      <Route path="home/listings" element={<Listings />} />
-      <Route path="my-listings/*" element={<MyListingsRoutes />} />
-      <Route path="my-reservations/*" element={<MyReservationsRoutes />} />
-      <Route path="messages/*" element={<MessagesRoutes />} />
-      <Route path="account/*" element={<AccountRoutes />} />
-      <Route path="admin-dashboard/*" element={<AdminDashboardRoutes />} />
+        <Route path="" element={<HomeTabsLayout />} >
+            <Route path="home/*" element={<Listings />} />
+            <Route path="my-listings/*" element={<MyListingsRoutes />} />
+            <Route path="my-reservations/*" element={<MyReservationsRoutes />} />
+            <Route path="messages/*" element={<MessagesRoutes />} />
+            <Route path="account/*" element={<AccountRoutes />} />
+            <Route path="admin-dashboard/*" element={<AdminDashboardRoutes />} />
+        </Route>
     </Routes>
   );
 }
---
+++ packages/ui-sharethrift/src/components/shared/apollo-connection.tsx
@@ -0,0 +1,52 @@
+import { from, ApolloLink, ApolloProvider } from "@apollo/client";
+import { type FC, useEffect } from "react";
+import { useAuth } from "react-oidc-context";
+// import { useParams } from "react-router-dom";
+import { ApolloLinkToAddAuthHeader, client, BaseApolloLink, TerminatingApolloLinkForGraphqlServer } from "./apollo-client-links"
+
+export interface ApolloConnectionProps {
+  children: React.ReactNode;
+}
+export const ApolloConnection: FC<ApolloConnectionProps> = (props: ApolloConnectionProps) => {
+  const auth = useAuth();
+//   const params = useParams(); // useParams.memberId won't work here because ApolloConnection wraps the Routes, not inside a Route
+
+
+  const apolloLinkChainForGraphqlDataSource = from([
+    BaseApolloLink(),
+    ApolloLinkToAddAuthHeader(auth),
+    // ApolloLinkToAddCustomHeader('community', communityId, (communityId !== 'accounts')),
+    // ApolloLinkToAddCustomHeader('member', memberId),
+    TerminatingApolloLinkForGraphqlServer({
+      uri: `${import.meta.env.VITE_FUNCTION_ENDPOINT}`,
+      batchMax: 15,
+      batchInterval: 50
+    })
+  ]);
+
+  const linkMap = {  
+    default: apolloLinkChainForGraphqlDataSource  
+  };  
+
+  const updateLink = () => {  
+    return ApolloLink.from([  
+      ApolloLink.split(  
+        // various options to split:
+        // 1. use a custom property in context: (operation) => operation.getContext().dataSource === some DataSourceEnum,
+        // 2. check for string name of the query if it is named: (operation) => operation.operationName === "CountryDetails",
+        (operation) => operation.operationName in linkMap,  
+        new ApolloLink((operation, forward) => {  
+          const link = linkMap[operation.operationName as keyof typeof linkMap] || linkMap.default;  
+          return link.request(operation, forward);  
+        }),  
+        apolloLinkChainForGraphqlDataSource  
+      )  
+    ]);  
+  }; 
+
+  useEffect(() => {
+    client.setLink(updateLink());
+  }, [auth]);
+
+  return <ApolloProvider client={client}>{props.children}</ApolloProvider>;
+};
