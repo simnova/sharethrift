6d1caf09da7a7347d177ae31a98a4db3b1b178a2
Jason Morais <jasonmorais02@gmail.com>
2025-08-06 08:31:50 -0400

    relative paths

--- packages/ui-sharethrift/src/components/layouts/signup/Index.tsx
+++ packages/ui-sharethrift/src/components/layouts/signup/Index.tsx
@@ -9,12 +9,12 @@ import Payment from './Payment';
 export default function SignupRoutes() {
   return (
     <Routes>
-      <Route path="/signup" element={<SignupIndex />} />
-      <Route path="/signup/select-account-type" element={<SelectAccountType />} />
-      <Route path="/signup/personal/terms" element={<Terms />} />
-      <Route path="/signup/personal/account-setup" element={<AccountSetup />} />
-      <Route path="/signup/personal/profile-setup" element={<ProfileSetup />} />
-      <Route path="/signup/personal/payment" element={<Payment />} />
+      <Route path="signup" element={<SignupIndex />} />
+      <Route path="signup/select-account-type" element={<SelectAccountType />} />
+      <Route path="signup/personal/terms" element={<Terms />} />
+      <Route path="signup/personal/account-setup" element={<AccountSetup />} />
+      <Route path="signup/personal/profile-setup" element={<ProfileSetup />} />
+      <Route path="signup/personal/payment" element={<Payment />} />
     </Routes>
   );
 }
