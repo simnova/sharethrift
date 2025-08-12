b67415c55fe3c4d0006a50d99db2a6171e3a6028
dev-kishor <kishornathgupta1999@gmail.com>
2025-08-12 23:42:44 +0530

    refactor: simplify Listings component and comment out Apollo query code

--- packages/ui-sharethrift/src/components/layouts/home/pages/Listings.tsx
+++ packages/ui-sharethrift/src/components/layouts/home/pages/Listings.tsx
@@ -1,28 +1,35 @@
-import { useQuery, gql } from "@apollo/client";
-import { useEffect } from "react";
-
-const FAKE_LISTINGS_QUERY = gql`
-  query FakeListings {
-    listings {
-      id
-      title
-      price
-    }
-  }
-`;
-
 export default function Listings() {
-  const { data, loading, error } = useQuery(FAKE_LISTINGS_QUERY);
-  useEffect(() => {
-    if (data) {
-      console.log("Fetched listings:", data.listings);
-    }
-  }, [data]);
+  return <div>Listings Page</div>;
+}
+
+
+
+// NOTE : ---------> Just for testing if you want to see connection (apollo) is fine
+// import { useQuery, gql } from "@apollo/client";
+// import { useEffect } from "react";
+
+// const FAKE_LISTINGS_QUERY = gql`
+//   query FakeListings {
+//     listings {
+//       id
+//       title
+//       price
+//     }
+//   }
+// `;
+
+// export default function Listings() {
+//   const { data, loading, error } = useQuery(FAKE_LISTINGS_QUERY);
+//   useEffect(() => {
+//     if (data) {
+//       console.log("Fetched listings:", data.listings);
+//     }
+//   }, [data]);
   
 
-  return (
-    <div>
-      <h2>Listings Page</h2>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>Listings Page</h2>
+//     </div>
+//   );
+// }
