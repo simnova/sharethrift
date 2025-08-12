aeb8035b0e49cfa29de7dba337304f253b9c0fcc
dev-kishor <kishornathgupta1999@gmail.com>
2025-08-12 23:41:10 +0530

    Apollo connection setup - inital

--- packages/ui-sharethrift/src/components/layouts/home/pages/Listings.tsx
+++ packages/ui-sharethrift/src/components/layouts/home/pages/Listings.tsx
@@ -1,3 +1,28 @@
+import { useQuery, gql } from "@apollo/client";
+import { useEffect } from "react";
+
+const FAKE_LISTINGS_QUERY = gql`
+  query FakeListings {
+    listings {
+      id
+      title
+      price
+    }
+  }
+`;
+
 export default function Listings() {
-  return <div>Listings Page</div>;
+  const { data, loading, error } = useQuery(FAKE_LISTINGS_QUERY);
+  useEffect(() => {
+    if (data) {
+      console.log("Fetched listings:", data.listings);
+    }
+  }, [data]);
+  
+
+  return (
+    <div>
+      <h2>Listings Page</h2>
+    </div>
+  );
 }
