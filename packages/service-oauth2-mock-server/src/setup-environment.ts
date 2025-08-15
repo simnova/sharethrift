554ad1539287fc2c897ec5f19d7b3b9ede1895fb
Jason Morais <jasonmorais02@gmail.com>
2025-08-15 14:25:11 -0400

    changed to be .env instead of sample

--- packages/service-oauth2-mock-server/src/setup-environment.ts
+++ packages/service-oauth2-mock-server/src/setup-environment.ts
@@ -1,9 +1,8 @@
-import fs from 'node:fs';
 import dotenv from 'dotenv';
 
 export const setupEnvironment = () => {
-    const envPath = fs.existsSync('.env.local') ? '.env.local' : '.env.sample';
-    console.log(`Loading environment variables from ${envPath}`);
-    dotenv.config({ path: envPath, override: true });
+    console.log('Setting up environment variables');
+    dotenv.config();
+    dotenv.config({ path: `.env.local`, override: true });
     console.log('Environment variables set up');
 }
