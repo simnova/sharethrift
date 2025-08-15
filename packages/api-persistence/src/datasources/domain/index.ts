5aa5a9de409cb30ac2aabb8e60555754d5a7687a
Evan Trinh <evannntrinh@gmail.com>
2025-08-15 10:12:00 -0400

    temp: fix backend 2

--- packages/api-persistence/src/datasources/domain/index.ts
+++ packages/api-persistence/src/datasources/domain/index.ts
@@ -1,25 +1,9 @@
 import type { MongooseSeedwork } from '@cellix/data-sources-mongoose';
 import type { DomainDataSource } from '@sthrift/api-domain';
-import { ItemListingPersistence } from '../../item-listing/index.ts';
 
 export const DomainDataSourceImplementation = (
 	initializedService: MongooseSeedwork.MongooseContextFactory,
 ): DomainDataSource => {
-	const itemListingPersistence = ItemListingPersistence(initializedService);
-	
-	return {
-		domainContexts: {
-			itemListing: {
-				getItemListingUnitOfWork: (
-					inProcEventBusInstance: unknown,
-					nodeEventBusInstance: unknown,
-				) => {
-					// For now, we ignore the event bus instances
-					// In a full implementation, these would be passed to the UoW
-					console.log('Event bus instances:', { inProcEventBusInstance, nodeEventBusInstance });
-					return itemListingPersistence.getItemListingUnitOfWork();
-				}
-			}
-		}
-	};
+	console.log(initializedService);
+	return {};
 };
