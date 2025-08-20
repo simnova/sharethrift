d6937e213cf497d4f508084e9d2342bf4702e2ba
Duy Nguyen <nkduy2010@gmail.com>
2025-08-20 09:57:09 -0400

    fix export

--- packages/api-domain/src/domain/contexts/user/index.ts
+++ packages/api-domain/src/domain/contexts/user/index.ts
@@ -1,4 +1,1 @@
-export * from './user.domain-permissions.ts';
-export * from './user.visa.ts';
-export * from './user.passport.ts';
-export * from './user/index.ts';
+export * as PersonalUser from './personal-user/index.ts';
