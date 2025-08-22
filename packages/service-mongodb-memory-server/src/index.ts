d4aaadce43ba27d163aafde9c8dd4691df1ce61e
Jason Morais <jasonmorais02@gmail.com>
2025-08-22 14:56:40 -0400

    port over replica set version of memory server

--- packages/service-mongodb-memory-server/src/index.ts
+++ packages/service-mongodb-memory-server/src/index.ts
@@ -1,16 +1,29 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import {setupEnvironment} from "./setup-environment.js";
+import { MongoMemoryReplSet } from 'mongodb-memory-server';
+import { setupEnvironment } from './setup-environment.js';
 
 setupEnvironment();
-console.log("Starting memory server on port ", process.env['port']);
-MongoMemoryServer.create({
-        instance: {
-            dbName: process.env['db_name'] ?? '',
-            port: Number(process.env['port']),
-        },
-}).then(server => {
-    console.log("Memory server started at ", server.getUri());
-}).catch(err => {
-    console.error("Error starting memory server: ", err);
-}); 
+// biome-ignore lint:useLiteralKeys
+const port = Number(process.env['PORT'] ?? 50000);
+// biome-ignore lint:useLiteralKeys
+const dbName = process.env['DB_NAME'] ?? 'test';
+// biome-ignore lint:useLiteralKeys
+const replSetName = process.env['REPL_SET_NAME'] ?? 'rs0';
 
+console.log('Starting MongoDB Memory Replica Set', { port, dbName, replSetName });
+
+    await MongoMemoryReplSet.create({
+        binary: { version: '7.0.14' },
+        replSet: {
+            name: replSetName,
+            count: 1,
+            storageEngine: 'wiredTiger'
+        },
+        instanceOpts: [{ port }]
+    }).then((replicaSet) => {
+
+        const uri = replicaSet.getUri(dbName);
+        console.log('MongoDB Memory Replica Set ready at:', uri);
+    }).catch((err) => {
+    console.error('Error starting MongoDB Memory Replica Set:', err);
+    process.exit(1);
+});
