31facd8ba9c42cb482520c50802b4d0ce471b6e7
dev-kishor <kishornathgupta1999@gmail.com>
2025-08-28 22:48:49 +0530

    add initial implementation of service-cybersource with payment and refund functionality

---
+++ packages/service-cybersource/src/index.ts
@@ -0,0 +1,63 @@
+import type { ServiceBase } from '@cellix/api-services-spec';
+import axios, { AxiosInstance } from 'axios';
+
+export interface PaymentRequest {
+  amount: number;
+  currency: string;
+  source: string;
+  description?: string;
+  [key: string]: any;
+}
+
+export interface PaymentResponse {
+  id: string;
+  status: string;
+  [key: string]: any;
+}
+
+export interface RefundRequest {
+  paymentId: string;
+  amount: number;
+  [key: string]: any;
+}
+
+export interface RefundResponse {
+  id: string;
+  status: string;
+  [key: string]: any;
+}
+
+export class ServicePayment implements ServiceBase<ServicePayment> {
+
+  private client: AxiosInstance | undefined;
+  private baseUrl: string;
+
+  constructor(baseUrl: string = process.env['PAYMENT_API_URL'] || 'http://localhost:4005') {
+    this.baseUrl = baseUrl;
+  }
+
+  public async startUp(): Promise<Exclude<ServicePayment, ServiceBase<ServicePayment>>> {
+    if (this.client) throw new Error('ServicePayment is already started');
+    this.client = axios.create({ baseURL: this.baseUrl });
+    return this as Exclude<ServicePayment, ServiceBase<ServicePayment>>;
+  }
+
+  public async shutdown(): Promise<void> {
+    this.client = undefined;
+  }
+
+  public get service(): AxiosInstance {
+    if (!this.client) throw new Error('ServicePayment is not started - cannot access service');
+    return this.client;
+  }
+
+  public async createPayment(req: PaymentRequest): Promise<PaymentResponse> {
+    const { data } = await this.service.post('/pts/v2/payments', req);
+    return data;
+  }
+
+  public async refundPayment(req: RefundRequest): Promise<RefundResponse> {
+    const { data } = await this.service.post('/pts/v2/refunds', req);
+    return data;
+  }
+}
