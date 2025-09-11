3e66fb2559ec04b47bba219573ca1d3a8e4c0567
Shannon Jaya <sjaya@intealth.org>
2025-09-11 11:32:04 -0400

    Update create listing stories

--- packages/ui-sharethrift/src/components/layouts/home/components/create-listing/create-listing.stories.tsx
+++ packages/ui-sharethrift/src/components/layouts/home/components/create-listing/create-listing.stories.tsx
@@ -2,32 +2,32 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { CreateListing } from './create-listing';
 
 const meta: Meta<typeof CreateListing> = {
-  title: 'Components/CreateListing',
-  component: CreateListing,
-  parameters: {
-    layout: 'fullscreen',
-  },
-  args: {
-    categories: [
-      'Electronics',
-      'Home & Garden', 
-      'Kids & Baby',
-      'Miscellaneous',
-      'Sports & Outdoors',
-      'Tools & Equipment',
-      'Vehicles & Transportation'
-    ],
-    isLoading: false,
-    onSubmit: () => {},
-    onCancel: () => {},
-    uploadedImages: [],
-    onImageAdd: () => {
-      // Mock image addition
-    },
-    onImageRemove: () => {
-      // Mock image removal
-    },
-  },
+	title: 'Components/CreateListing',
+	component: CreateListing,
+	parameters: {
+		layout: 'fullscreen',
+	},
+	args: {
+		categories: [
+			'Electronics',
+			'Home & Garden',
+			'Kids & Baby',
+			'Miscellaneous',
+			'Sports & Outdoors',
+			'Tools & Equipment',
+			'Vehicles & Transportation',
+		],
+		isLoading: false,
+		onSubmit: () => {},
+		onCancel: () => {},
+		uploadedImages: [],
+		onImageAdd: () => {
+			// Mock image addition
+		},
+		onImageRemove: () => {
+			// Mock image removal
+		},
+	},
 };
 
 export default meta;
@@ -36,17 +36,11 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {};
 
 export const WithImages: Story = {
-  args: {
-    uploadedImages: [
-      '/assets/item-images/bike.png',
-      '/assets/item-images/tent.png',
-      '/assets/item-images/projector.png',
-    ],
-  },
-};
-
-export const Loading: Story = {
-  args: {
-    isLoading: true,
-  },
+	args: {
+		uploadedImages: [
+			'/assets/item-images/bike.png',
+			'/assets/item-images/tent.png',
+			'/assets/item-images/projector.png',
+		],
+	},
 };
