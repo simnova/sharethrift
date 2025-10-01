ea6e3d116c4a25cde22ea7f79e8b0466c9c7c898
Duy Nguyen <nkduy2010@gmail.com>
2025-10-01 09:39:47 -0400

    add missing dependency for useEffect

--- apps/ui-sharethrift/src/components/shared/require-auth.tsx
+++ apps/ui-sharethrift/src/components/shared/require-auth.tsx
@@ -1,69 +1,53 @@
-import { useEffect, type JSX } from 'react';
-import { hasAuthParams, useAuth } from 'react-oidc-context';
-import { Navigate } from 'react-router-dom';
+import { useEffect, type JSX } from "react";
+import { hasAuthParams, useAuth } from "react-oidc-context";
+import { Navigate } from "react-router-dom";
 
 interface RequireAuthProps {
-	children: JSX.Element;
-	redirectPath: string;
-	forceLogin?: boolean;
+  children: JSX.Element;
+  redirectPath: string;
+  forceLogin?: boolean;
 }
 
 export const RequireAuth: React.FC<RequireAuthProps> = (props) => {
-	const auth = useAuth();
+  const auth = useAuth();
 
-	// automatically sign-in
-	useEffect(() => {
-		if (
-			!hasAuthParams() &&
-			props.forceLogin === true &&
-			!auth.isAuthenticated &&
-			!auth.activeNavigator &&
-			!auth.isLoading &&
-			!auth.error
-		) {
-			window.sessionStorage.setItem(
-				'redirectTo',
-				`${location.pathname}${location.search}`,
-			);
+  // automatically sign-in
+  useEffect(() => {
+    if (!hasAuthParams() && props.forceLogin === true && !auth.isAuthenticated && !auth.activeNavigator && !auth.isLoading && !auth.error) {
+      window.sessionStorage.setItem("redirectTo", `${location.pathname}${location.search}`);
 
-			auth.signinRedirect();
-		}
-	}, [
-		auth.isAuthenticated,
-		auth.activeNavigator,
-		auth.isLoading,
-		auth.signinRedirect,
-		auth.error,
-	]);
+      auth.signinRedirect();
+    }
+  }, [auth.isAuthenticated, auth.activeNavigator, auth.isLoading, auth.signinRedirect, auth.error, props.forceLogin]);
 
-	// automatically refresh token
-	useEffect(() => {
-		return auth.events.addAccessTokenExpiring(() => {
-			auth.signinSilent({
-				redirect_uri: import.meta.env['VITE_B2C_REDIRECT_URI'] ?? '',
-			});
-		});
+  // automatically refresh token
+  useEffect(() => {
+    return auth.events.addAccessTokenExpiring(() => {
+      auth.signinSilent({
+        redirect_uri: import.meta.env["VITE_B2C_REDIRECT_URI"] ?? "",
+      });
+    });
 
-		// *** Suggestion from sourcery that needs investigation
-		// const handleAccessTokenExpiring = () => {
-		//   auth.signinSilent({
-		//     redirect_uri: import.meta.env.VITE_B2C_REDIRECT_URI ?? "",
-		//   });
-		// };
-		// auth.events.addAccessTokenExpiring(handleAccessTokenExpiring);
-		// return () => {
-		//   auth.events.removeAccessTokenExpiring(handleAccessTokenExpiring);
-		// };
-	}, [auth.events, auth.signinSilent]);
+    // *** Suggestion from sourcery that needs investigation
+    // const handleAccessTokenExpiring = () => {
+    //   auth.signinSilent({
+    //     redirect_uri: import.meta.env.VITE_B2C_REDIRECT_URI ?? "",
+    //   });
+    // };
+    // auth.events.addAccessTokenExpiring(handleAccessTokenExpiring);
+    // return () => {
+    //   auth.events.removeAccessTokenExpiring(handleAccessTokenExpiring);
+    // };
+  }, [auth.events, auth.signinSilent]);
 
-	let result: JSX.Element;
-	if (auth.isAuthenticated) {
-		result = props.children;
-	} else if (auth.error) {
-		result = <Navigate to="/" />;
-	} else {
-		return <div>Checking auth2...</div>;
-	}
+  let result: JSX.Element;
+  if (auth.isAuthenticated) {
+    result = props.children;
+  } else if (auth.error) {
+    result = <Navigate to="/" />;
+  } else {
+    return <div>Checking auth2...</div>;
+  }
 
-	return result;
+  return result;
 };
