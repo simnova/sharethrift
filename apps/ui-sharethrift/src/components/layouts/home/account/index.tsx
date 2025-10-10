8bb049c321adb202297114e9c85f0155fd5b5841
Lian <ljung@intealth.org>
2025-10-10 13:10:22 -0400

    separated profile and settings folders

--- apps/ui-sharethrift/src/components/layouts/home/account/index.tsx
+++ apps/ui-sharethrift/src/components/layouts/home/account/index.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
-import EditSettings from './pages/EditSettings.tsx';
-import Profile from './pages/Profile.tsx';
-import Settings from './pages/Settings.tsx';
+import EditSettings from './settings/pages/EditSettings.tsx';
+import Profile from './profile/pages/Profile.tsx';
+import Settings from './settings/pages/Settings.tsx';
 import AdminDashboardMain from './admin-dashboard/pages/admin-dashboard-main.tsx';
 
 export default function AccountRoutes() {
