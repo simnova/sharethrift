0bc090a12cc1628f06b61de7767f15957e8f2793
Duy Nguyen <nkduy2010@gmail.com>
2025-10-14 11:09:06 -0400

    add style to be consistent to other fields on the form

--- apps/ui-sharethrift/public/scripts/microform-css.js
+++ apps/ui-sharethrift/public/scripts/microform-css.js
@@ -1,6 +1,7 @@
 // get only Microform iframes (e.g., by id, class, or src)
 for (const element of document.getElementsByTagName('iframe')) {
 	element.style.height = '30px';
-	// element.style.border = '1px solid #d5d0da';
+	element.style.border = '1px solid #d9d9d9';
 	element.style.paddingLeft = '10px';
+	element.style.borderRadius = '3px';
 }
