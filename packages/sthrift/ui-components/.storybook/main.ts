dc98502b4bf5e08216d25b24e87abfca1c645b08
Lian <ljung@intealth.org>
2025-10-16 13:28:28 -0400

    removed storybook-addon-apollo-client

--- packages/sthrift/ui-components/.storybook/main.ts
+++ packages/sthrift/ui-components/.storybook/main.ts
@@ -16,7 +16,6 @@ const config: StorybookConfig = {
 		getAbsolutePath('@storybook/addon-onboarding'),
 		getAbsolutePath('@storybook/addon-a11y'),
 		getAbsolutePath('@storybook/addon-vitest'),
-		getAbsolutePath('storybook-addon-apollo-client'),
 	],
 	framework: {
 		name: getAbsolutePath('@storybook/react-vite'),
