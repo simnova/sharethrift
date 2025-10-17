1c74922563de2dcb0f52b4186233b117dc2acaec
Duy Nguyen <nkduy2010@gmail.com>
2025-10-17 11:42:42 -0400

    add missing export

--- packages/sthrift/domain/src/domain/contexts/account-plan/account-plan/index.ts
+++ packages/sthrift/domain/src/domain/contexts/account-plan/account-plan/index.ts
@@ -5,3 +5,4 @@ export type {
 } from './account-plan.entity.ts';
 export type { AccountPlanRepository } from './account-plan.repository.ts';
 export type { AccountPlanUnitOfWork } from './account-plan.uow.ts';
+export type { AccountPlanFeatureProps } from './account-plan-feature.entity.ts';
