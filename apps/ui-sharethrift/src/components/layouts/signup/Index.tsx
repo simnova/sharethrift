caec112bd3e464e576ff7f1095f2123079ba27ae
Duy Nguyen <nkduy2010@gmail.com>
2025-11-14 14:10:49 -0500

    refactor: remove unnecessary component

--- apps/ui-sharethrift/src/components/layouts/signup/Index.tsx
+++ apps/ui-sharethrift/src/components/layouts/signup/Index.tsx
@@ -3,13 +3,13 @@ import { SelectAccountTypePage } from "./pages/select-account-type-page.tsx";
 import { AccountSetupPage } from "./pages/account-setup-page.tsx";
 import { ProfileSetupPage } from "./pages/profile-setup-page.tsx";
 import { PaymentPage } from "./pages/payment-page.tsx";
-import { SectionLayoutContainer } from "./section-layout.container.tsx";
+import { SectionLayout } from "./section-layout.tsx";
 import { TermsPage } from "./pages/terms-page.tsx";
 
 export const SignupRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route element={<SectionLayoutContainer />}>
+      <Route element={<SectionLayout />}>
         <Route path="select-account-type" element={<SelectAccountTypePage />} />
         <Route path="account-setup" element={<AccountSetupPage />} />
         <Route path="profile-setup" element={<ProfileSetupPage />} />
