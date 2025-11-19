89f9ee73683272689299435b1131ae6cf6347ae0
Kerry Lin <kerry.lin3690@gmail.com>
2025-11-19 11:00:52 -0500

    Revert "fix: allow application-services tests to pass with no test files"
    This reverts commit d31771cc01d53d63515bf616f815f51412efee79.

--- packages/sthrift/application-services/vitest.config.ts
+++ packages/sthrift/application-services/vitest.config.ts
@@ -1,8 +1,3 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
 import { nodeConfig } from '@cellix/vitest-config';
 
-export default mergeConfig(nodeConfig, defineConfig({
-  test: {
-    passWithNoTests: true,
-  },
-}));
+export default nodeConfig;
