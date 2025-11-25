ed2f5e2fb95b54ccc40a0206c6e6627b24cf795f
Kerry Lin <kerry.lin3690@gmail.com>
2025-11-25 17:37:11 -0500

    CHORE: Delete unused files identified by Knip

--- packages/sthrift/domain/src/domain/domain-execution-context.ts
+++
@@ -1,7 +0,0 @@
-import type { DomainSeedwork } from '@cellix/domain-seedwork';
-import type { Passport } from './contexts/passport.ts';
-
-export interface DomainExecutionContext
-	extends DomainSeedwork.BaseDomainExecutionContext {
-	passport: Passport;
-}
