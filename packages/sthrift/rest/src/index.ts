cc35298d6ad1330d7db0f284f82cdc78f94d74e2
Rohit Kumar <rkumar@ecfmg.org>
2025-12-05 01:07:09 +0530

    Fix sonar issues

--- packages/sthrift/rest/src/index.ts
+++ packages/sthrift/rest/src/index.ts
@@ -13,8 +13,9 @@ export type HttpHandler = (
 export const restHandlerCreator = (applicationServicesFactory: ApplicationServicesFactory): HttpHandler => {
 	return async (request: HttpRequest, _context: InvocationContext) => {
 		const rawAuthHeader = request.headers.get('Authorization') ?? undefined;
+		// biome-ignore lint/complexity/useLiteralKeys: Required for index signature access in TypeScript with noPropertyAccessFromIndexSignature
 		const hints: PrincipalHints = {
-			memberId: request.params[`memberId`] ?? undefined,
+			memberId: request.params['memberId'] ?? undefined,
 			communityId: request.params['communityId'] ?? undefined,
 		};
 		const applicationServices = await applicationServicesFactory.forRequest(rawAuthHeader, hints);
