722fabf2fd9485e18401b47131c0e60d7906e877
Kerry Lin <kerry.lin3690@gmail.com>
2025-12-04 22:17:08 -0500

    add ons to previous commit

--- packages/sthrift/domain/src/domain/contexts/user/personal-user/personal-user.value-objects.ts
+++ packages/sthrift/domain/src/domain/contexts/user/personal-user/personal-user.value-objects.ts
@@ -1,4 +1,6 @@
+import { VOOptional } from '@lucaspaganini/value-objects';
 import {
+	Email as EmailBase,
 	Username as UsernameBase,
 	FirstName as FirstNameBase,
 	LastName as LastNameBase,
@@ -13,7 +15,29 @@ import {
  * Value objects for User aggregate validation and data integrity.
  */
 
+/**
+ * User type enumeration
+ */
+export const UserType = {
+	PERSONAL: 'personal',
+	ADMIN: 'admin',
+} as const;
+
+export type UserType = (typeof UserType)[keyof typeof UserType];
+
+/**
+ * Account type enumeration
+ */
+export const AccountType = {
+	PERSONAL: 'personal',
+	BUSINESS: 'business',
+	ENTERPRISE: 'enterprise',
+} as const;
+
+export type AccountType = (typeof AccountType)[keyof typeof AccountType];
+
 // Re-export shared value objects
+export class Email extends VOOptional(EmailBase, [undefined]) {}
 export class Username extends UsernameBase {}
 export class FirstName extends FirstNameBase {}
 export class LastName extends LastNameBase {}
@@ -22,4 +46,3 @@ export class City extends CityBase {}
 export class State extends StateBase {}
 export class Country extends CountryBase {}
 export class ZipCode extends ZipCodeBase {}
-
