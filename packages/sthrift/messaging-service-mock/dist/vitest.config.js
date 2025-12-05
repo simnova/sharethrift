4480a9d3828116d6b76e9b910bfd0a92c6484850
dev-kishor <kishornathgupta1999@gmail.com>
2025-12-05 22:53:16 +0530

    removed node_modules, and dist

--- packages/sthrift/messaging-service-mock/dist/vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
-import { nodeConfig } from '@cellix/vitest-config';
-export default mergeConfig(nodeConfig, defineConfig({
-    test: {
-        testTimeout: 10000,
-    },
-}));
-//# sourceMappingURL=vitest.config.js.map
