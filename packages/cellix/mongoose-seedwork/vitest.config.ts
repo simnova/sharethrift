3d3929e9918e10851c87c2b4809dc8a9f92a527b
Rohit Kumar <rkumar@ecfmg.org>
2025-12-08 20:49:53 +0530

    Remove unneccessary config settings

--- packages/cellix/mongoose-seedwork/vitest.config.ts
+++ packages/cellix/mongoose-seedwork/vitest.config.ts
@@ -6,7 +6,7 @@ export default mergeConfig(
 	defineConfig({
 		// Add package-specific overrides here if needed
 		test: {
-			include: ['src/**/*.test.ts'],
+			include: ['src/**/*.test.ts', 'tests/integration/**/*.test.ts'],
 			retry: 0,
 			coverage: {
 				exclude: ['**/index.ts', '**/base.ts'],
