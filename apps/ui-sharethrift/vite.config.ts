7e0a5bd903101652761f620dd94681d54e2b4206
Duy Nguyen <nkduy2010@gmail.com>
2025-12-10 15:24:47 -0500

    attempt to fix error on pipeline

--- apps/ui-sharethrift/vite.config.ts
+++ apps/ui-sharethrift/vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		open: true, // Automatically open browser on server start
+    watch: { usePolling: true }, // attempt to fix issue on pipeline: [vite] (client) Pre-transform error: EMFILE: too many open files, open '/home/vsts/work/1/s/packages/sthrift/ui-components/dist/src/index.js'
 	},
 });
