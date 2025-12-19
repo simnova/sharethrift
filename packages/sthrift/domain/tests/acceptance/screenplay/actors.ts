12355ab9aace71b68364c30b96d47dbc897e923b
Anya Patel <anya.pat06@gmail.com>
2025-12-19 14:40:08 -0500

    removed GraphQL-dependent acceptance tests from domain package

--- packages/sthrift/domain/tests/acceptance/screenplay/actors.ts
+++
@@ -1,9 +0,0 @@
-import { actorCalled } from '@serenity-js/core';
-
-/**
- * Creates an actor that can interact with the ShareThrift API.
- * actors: represent the users or systems that perform actions.
- */
-
-export const ActorNamed = (name: string) =>
-  actorCalled(name);
