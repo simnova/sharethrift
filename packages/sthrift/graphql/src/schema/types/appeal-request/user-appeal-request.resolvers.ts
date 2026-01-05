110987cc57af7e2399c57986ae9ba85b7576e2b0
dev-kishor <kishornathgupta1999@gmail.com>
2026-01-05 20:10:48 +0530

    fixed create-appeal request for block user

--- packages/sthrift/graphql/src/schema/types/appeal-request/user-appeal-request.resolvers.ts
+++ packages/sthrift/graphql/src/schema/types/appeal-request/user-appeal-request.resolvers.ts
@@ -1,87 +1,88 @@
 import type { GraphContext } from '../../../init/context.ts';
 import type { GraphQLResolveInfo } from 'graphql';
 import type {
-	Resolvers,
-	QueryGetUserAppealRequestArgs,
-	QueryGetAllUserAppealRequestsArgs,
-	MutationCreateUserAppealRequestArgs,
-	MutationUpdateUserAppealRequestStateArgs,
+    Resolvers,
+    QueryGetUserAppealRequestArgs,
+    QueryGetAllUserAppealRequestsArgs,
+    MutationCreateUserAppealRequestArgs,
+    MutationUpdateUserAppealRequestStateArgs,
 } from '../../builder/generated.ts';
 import { PopulateUserFromField } from '../../resolver-helper.ts';
 
 const userAppealRequestResolvers: Resolvers = {
-	UserAppealRequest: {
-		user: PopulateUserFromField('user'),
-		blocker: PopulateUserFromField('blocker'),
-	},
-	Query: {
-		getUserAppealRequest: async (
-			_parent: unknown,
-			args: QueryGetUserAppealRequestArgs,
-			context: GraphContext,
-			_info: GraphQLResolveInfo,
-		) => {
-			// TODO: SECURITY - Add authentication and authorization check
-			// Should verify user has permission to view this appeal request
-			return await context.applicationServices.AppealRequest.UserAppealRequest.getById(
-				args,
-			);
-		},
-		getAllUserAppealRequests: async (
-			_parent: unknown,
-			args: QueryGetAllUserAppealRequestsArgs,
-			context: GraphContext,
-			_info: GraphQLResolveInfo,
-		) => {
-			// TODO: SECURITY - Add admin permission check
-			// Only admins should be able to view all user appeal requests
-			return await context.applicationServices.AppealRequest.UserAppealRequest.getAll(
-				{
-					page: args.input.page,
-					pageSize: args.input.pageSize,
-					...(args.input.stateFilters && {
-						stateFilters: [...args.input.stateFilters],
-					}),
-					...(args.input.sorter && { sorter: args.input.sorter }),
-				},
-			);
-		},
-	},
+    UserAppealRequest: {
+        type: () => 'USER',
+        user: PopulateUserFromField('user'),
+        blocker: PopulateUserFromField('blocker'),
+    },
+    Query: {
+        getUserAppealRequest: async (
+            _parent: unknown,
+            args: QueryGetUserAppealRequestArgs,
+            context: GraphContext,
+            _info: GraphQLResolveInfo,
+        ) => {
+            // TODO: SECURITY - Add authentication and authorization check
+            // Should verify user has permission to view this appeal request
+            return await context.applicationServices.AppealRequest.UserAppealRequest.getById(
+                args,
+            );
+        },
+        getAllUserAppealRequests: async (
+            _parent: unknown,
+            args: QueryGetAllUserAppealRequestsArgs,
+            context: GraphContext,
+            _info: GraphQLResolveInfo,
+        ) => {
+            // TODO: SECURITY - Add admin permission check
+            // Only admins should be able to view all user appeal requests
+            return await context.applicationServices.AppealRequest.UserAppealRequest.getAll(
+                {
+                    page: args.input.page,
+                    pageSize: args.input.pageSize,
+                    ...(args.input.stateFilters && {
+                        stateFilters: [...args.input.stateFilters],
+                    }),
+                    ...(args.input.sorter && { sorter: args.input.sorter }),
+                },
+            );
+        },
+    },
 
-	Mutation: {
-		createUserAppealRequest: async (
-			_parent: unknown,
-			args: MutationCreateUserAppealRequestArgs,
-			context: GraphContext,
-			_info: GraphQLResolveInfo,
-		) => {
-			// TODO: SECURITY - Add authentication check
-			// TODO: SECURITY - Verify the authenticated user matches userId
-			// TODO: SECURITY - Verify the user is actually blocked by blockerId
-			return await context.applicationServices.AppealRequest.UserAppealRequest.create(
-				args.input,
-			);
-		},
-		updateUserAppealRequestState: async (
-			_parent: unknown,
-			args: MutationUpdateUserAppealRequestStateArgs,
-			context: GraphContext,
-			_info: GraphQLResolveInfo,
-		) => {
-			// TODO: SECURITY - Add admin permission check
-			// Only admins should be able to update appeal request state
-			const state = args.input.state.toLowerCase() as
-				| 'requested'
-				| 'denied'
-				| 'accepted';
-			return await context.applicationServices.AppealRequest.UserAppealRequest.updateState(
-				{
-					id: args.input.id,
-					state,
-				},
-			);
-		},
-	},
+    Mutation: {
+        createUserAppealRequest: async (
+            _parent: unknown,
+            args: MutationCreateUserAppealRequestArgs,
+            context: GraphContext,
+            _info: GraphQLResolveInfo,
+        ) => {
+            // TODO: SECURITY - Add authentication check
+            // TODO: SECURITY - Verify the authenticated user matches userId
+            // TODO: SECURITY - Verify the user is actually blocked by blockerId
+            return await context.applicationServices.AppealRequest.UserAppealRequest.create(
+                args.input,
+            );
+        },
+        updateUserAppealRequestState: async (
+            _parent: unknown,
+            args: MutationUpdateUserAppealRequestStateArgs,
+            context: GraphContext,
+            _info: GraphQLResolveInfo,
+        ) => {
+            // TODO: SECURITY - Add admin permission check
+            // Only admins should be able to update appeal request state
+            const state = args.input.state.toLowerCase() as
+                | 'requested'
+                | 'denied'
+                | 'accepted';
+            return await context.applicationServices.AppealRequest.UserAppealRequest.updateState(
+                {
+                    id: args.input.id,
+                    state,
+                },
+            );
+        },
+    },
 };
 
 export default userAppealRequestResolvers;
--- packages/sthrift/persistence/src/datasources/domain/appeal-request/user-appeal-request/user-appeal-request.domain-adapter.ts
+++ packages/sthrift/persistence/src/datasources/domain/appeal-request/user-appeal-request/user-appeal-request.domain-adapter.ts
@@ -5,112 +5,116 @@ import { MongooseSeedwork } from '@cellix/mongoose-seedwork';
 import { PersonalUserDomainAdapter } from '../../user/personal-user/personal-user.domain-adapter.ts';
 
 export class UserAppealRequestConverter extends MongooseSeedwork.MongoTypeConverter<
-	Models.AppealRequest.UserAppealRequest,
-	UserAppealRequestDomainAdapter,
-	Domain.Passport,
-	Domain.Contexts.AppealRequest.UserAppealRequest.UserAppealRequest<UserAppealRequestDomainAdapter>
+    Models.AppealRequest.UserAppealRequest,
+    UserAppealRequestDomainAdapter,
+    Domain.Passport,
+    Domain.Contexts.AppealRequest.UserAppealRequest.UserAppealRequest<UserAppealRequestDomainAdapter>
 > {
-	constructor() {
-		super(
-			UserAppealRequestDomainAdapter,
-			Domain.Contexts.AppealRequest.UserAppealRequest
-				.UserAppealRequest<UserAppealRequestDomainAdapter>,
-		);
-	}
+    constructor() {
+        super(
+            UserAppealRequestDomainAdapter,
+            Domain.Contexts.AppealRequest.UserAppealRequest
+                .UserAppealRequest<UserAppealRequestDomainAdapter>,
+        );
+    }
 }
 
 export class UserAppealRequestDomainAdapter
-	extends MongooseSeedwork.MongooseDomainAdapter<Models.AppealRequest.UserAppealRequest>
-	implements
-		Domain.Contexts.AppealRequest.UserAppealRequest.UserAppealRequestProps
+    extends MongooseSeedwork.MongooseDomainAdapter<Models.AppealRequest.UserAppealRequest>
+    implements
+        Domain.Contexts.AppealRequest.UserAppealRequest.UserAppealRequestProps
 {
-	get user(): Domain.Contexts.User.PersonalUser.PersonalUserEntityReference {
-		if (!this.doc.user) {
-			throw new Error('user is not populated');
-		}
-		if (this.doc.user instanceof MongooseSeedwork.ObjectId) {
-			throw new TypeError('user is not populated');
-		}
-		const adapter = new PersonalUserDomainAdapter(
-			this.doc.user as Models.User.PersonalUser,
-		);
-		return adapter.entityReference;
-	}
+    get user(): Domain.Contexts.User.PersonalUser.PersonalUserEntityReference {
+        if (!this.doc.user) {
+            throw new Error('user is not populated');
+        }
+        if (this.doc.user instanceof MongooseSeedwork.ObjectId) {
+            return {
+                id: this.doc.user.toString(),
+            } as Domain.Contexts.User.PersonalUser.PersonalUserEntityReference;
+        }
+        const adapter = new PersonalUserDomainAdapter(
+            this.doc.user as Models.User.PersonalUser,
+        );
+        return adapter.entityReference;
+    }
 
-	async loadUser(): Promise<Domain.Contexts.User.PersonalUser.PersonalUserEntityReference> {
-		if (!this.doc.user) {
-			throw new Error('user is not populated');
-		}
-		if (this.doc.user instanceof MongooseSeedwork.ObjectId) {
-			await this.doc.populate('user');
-		}
-		const adapter = new PersonalUserDomainAdapter(
-			this.doc.user as Models.User.PersonalUser,
-		);
-		return adapter.entityReference;
-	}
+    async loadUser(): Promise<Domain.Contexts.User.PersonalUser.PersonalUserEntityReference> {
+        if (!this.doc.user) {
+            throw new Error('user is not populated');
+        }
+        if (this.doc.user instanceof MongooseSeedwork.ObjectId) {
+            await this.doc.populate('user');
+        }
+        const adapter = new PersonalUserDomainAdapter(
+            this.doc.user as Models.User.PersonalUser,
+        );
+        return adapter.entityReference;
+    }
 
-	set user(user:
-		| Domain.Contexts.User.PersonalUser.PersonalUserEntityReference
-		| Domain.Contexts.User.PersonalUser.PersonalUser<PersonalUserDomainAdapter>,) {
-		if (!user?.id) {
-			throw new Error('user reference is missing id');
-		}
-		this.doc.set('user', new MongooseSeedwork.ObjectId(user.id));
-	}
+    set user(user:
+        | Domain.Contexts.User.PersonalUser.PersonalUserEntityReference
+        | Domain.Contexts.User.PersonalUser.PersonalUser<PersonalUserDomainAdapter>,) {
+        if (!user?.id) {
+            throw new Error('user reference is missing id');
+        }
+        this.doc.set('user', new MongooseSeedwork.ObjectId(user.id));
+    }
 
-	get blocker(): Domain.Contexts.User.PersonalUser.PersonalUserEntityReference {
-		if (!this.doc.blocker) {
-			throw new Error('blocker is not populated');
-		}
-		if (this.doc.blocker instanceof MongooseSeedwork.ObjectId) {
-			throw new TypeError('blocker is not populated');
-		}
-		const adapter = new PersonalUserDomainAdapter(
-			this.doc.blocker as Models.User.PersonalUser,
-		);
-		return adapter.entityReference;
-	}
+    get blocker(): Domain.Contexts.User.PersonalUser.PersonalUserEntityReference {
+        if (!this.doc.blocker) {
+            throw new Error('blocker is not populated');
+        }
+        if (this.doc.blocker instanceof MongooseSeedwork.ObjectId) {
+            return {
+                id: this.doc.blocker.toString(),
+            } as Domain.Contexts.User.PersonalUser.PersonalUserEntityReference;
+        }
+        const adapter = new PersonalUserDomainAdapter(
+            this.doc.blocker as Models.User.PersonalUser,
+        );
+        return adapter.entityReference;
+    }
 
-	async loadBlocker(): Promise<Domain.Contexts.User.PersonalUser.PersonalUserEntityReference> {
-		if (!this.doc.blocker) {
-			throw new Error('blocker is not populated');
-		}
-		if (this.doc.blocker instanceof MongooseSeedwork.ObjectId) {
-			await this.doc.populate('blocker');
-		}
-		const adapter = new PersonalUserDomainAdapter(
-			this.doc.blocker as Models.User.PersonalUser,
-		);
-		return adapter.entityReference;
-	}
+    async loadBlocker(): Promise<Domain.Contexts.User.PersonalUser.PersonalUserEntityReference> {
+        if (!this.doc.blocker) {
+            throw new Error('blocker is not populated');
+        }
+        if (this.doc.blocker instanceof MongooseSeedwork.ObjectId) {
+            await this.doc.populate('blocker');
+        }
+        const adapter = new PersonalUserDomainAdapter(
+            this.doc.blocker as Models.User.PersonalUser,
+        );
+        return adapter.entityReference;
+    }
 
-	set blocker(blocker:
-		| Domain.Contexts.User.PersonalUser.PersonalUserEntityReference
-		| Domain.Contexts.User.PersonalUser.PersonalUser<PersonalUserDomainAdapter>,) {
-		if (!blocker?.id) {
-			throw new Error('blocker reference is missing id');
-		}
-		this.doc.set('blocker', new MongooseSeedwork.ObjectId(blocker.id));
-	}
+    set blocker(blocker:
+        | Domain.Contexts.User.PersonalUser.PersonalUserEntityReference
+        | Domain.Contexts.User.PersonalUser.PersonalUser<PersonalUserDomainAdapter>,) {
+        if (!blocker?.id) {
+            throw new Error('blocker reference is missing id');
+        }
+        this.doc.set('blocker', new MongooseSeedwork.ObjectId(blocker.id));
+    }
 
-	get reason(): string {
-		return this.doc.reason;
-	}
+    get reason(): string {
+        return this.doc.reason;
+    }
 
-	set reason(value: string) {
-		this.doc.reason = value;
-	}
+    set reason(value: string) {
+        this.doc.reason = value;
+    }
 
-	get state(): string {
-		return this.doc.state;
-	}
+    get state(): string {
+        return this.doc.state;
+    }
 
-	set state(value: string) {
-		this.doc.state = value as 'requested' | 'denied' | 'accepted';
-	}
+    set state(value: string) {
+        this.doc.state = value as 'requested' | 'denied' | 'accepted';
+    }
 
-	get type(): string {
-		return this.doc.type;
-	}
+    get type(): string {
+        return this.doc.type;
+    }
 }
