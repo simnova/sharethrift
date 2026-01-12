de96a0990fe3518191b7d7aff518ebf99a7bff35
Duy Nguyen <nkduy2010@gmail.com>
2026-01-12 16:17:43 -0500

    hide stories containing functional tests only

--- apps/ui-sharethrift/src/components/shared/stories/require-auth.stories.tsx
+++ apps/ui-sharethrift/src/components/shared/stories/require-auth.stories.tsx
@@ -11,6 +11,8 @@ const meta: Meta<typeof RequireAuth> = {
 	parameters: {
 		layout: 'centered',
 	},
+  tags: ['!dev'], // not rendered in sidebar - https://storybook.js.org/docs/writing-stories/tags
+
 };
 
 export default meta;
